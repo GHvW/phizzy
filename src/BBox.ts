3d0965a7884303c195410b16ee55c16fdf29dae6
Garrett van Wageningen <ghvanw283@gmail.com>
2025-01-22 21:01:27 -0600

    initish commit

---
+++ src/BBox.ts
@@ -0,0 +1,20 @@
+import { Vec2 } from "dim-sum";
+
+export class BBox {
+
+    min: Vec2;
+    max: Vec2;
+
+    constructor(min: Vec2, max: Vec2) {
+        this.min = min;
+        this.max = max;
+    }
+
+    intersects(other: BBox): boolean {
+        return this.max.x >= other.min.x
+            && this.min.y <= other.max.x
+            && this.max.y >= other.min.y
+            && this.min.y <= other.max.y
+    }
+}
+
